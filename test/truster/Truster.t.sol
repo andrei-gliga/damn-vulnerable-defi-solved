39fb83dc1cd7d05c0f2e851c3bbe88a754e4b94e
hyp3rion123 <andreigli@yahoo.com>
2025-03-05 11:03:47 +0100

    solved truster and side-entrance

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -5,6 +5,7 @@ pragma solidity =0.8.25;
 import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
+import {TrusterExploit} from "../truster/TrusterExploit.sol";
 
 contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -51,7 +52,8 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        TrusterExploit t = new TrusterExploit(address(token), address(pool), recovery);
+        t.attack();
     }
 
     /**
