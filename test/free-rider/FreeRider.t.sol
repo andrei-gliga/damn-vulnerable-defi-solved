f848c283ce501fbcc00ed4c4b9706f441d2dcace
hyp3rion123 <andreigli@yahoo.com>
2025-03-17 21:07:54 +0100

    solved free rider nft marketplace

--- test/free-rider/FreeRider.t.sol
+++ test/free-rider/FreeRider.t.sol
@@ -11,6 +11,7 @@ import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {FreeRiderNFTMarketplace} from "../../src/free-rider/FreeRiderNFTMarketplace.sol";
 import {FreeRiderRecoveryManager} from "../../src/free-rider/FreeRiderRecoveryManager.sol";
 import {DamnValuableNFT} from "../../src/DamnValuableNFT.sol";
+import {FreeRiderExploiter} from "./FreeRiderExploiter.sol";
 
 contract FreeRiderChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -123,7 +124,16 @@ contract FreeRiderChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_freeRider() public checkSolvedByPlayer {
-        
+        FreeRiderExploiter exploit = new FreeRiderExploiter(
+            weth,
+            uniswapPair,
+            marketplace,
+            recoveryManager,
+            nft
+        );
+
+        exploit.attack();
+
     }
 
     /**
